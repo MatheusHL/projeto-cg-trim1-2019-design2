e083c49ef36cb1fc7e10d130565013d3699cd2a9
MatheusHL <matheuscamposhl@gmail.com>
2019-04-16 20:10:39 -0300

    Contando moedas

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.CompareTag("Coin")){
+        if (other.gameObject.CompareTag("Coin")){
             SFXManager.instance.ShowCoinParticles(other.gameObject);
             AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
+            Scenemaneger.instance.IncrementCoinCount();
         }
     }
 
--- Assets/Scripts/Scenemaneger.cs
+++ Assets/Scripts/Scenemaneger.cs
@@ -5,6 +5,8 @@ using UnityEngine.Assertions;
 
 public class Scenemaneger : MonoBehaviour
 {
+    public static Scenemaneger instance = null;
+
     [SerializeField]
     private Vector3 TapeSpeed = new Vector3(-2f, 0f ,0f);
     [SerializeField]
@@ -12,19 +14,30 @@ public class Scenemaneger : MonoBehaviour
 
     public UIComponents uiComponents;
 
+    SceneData sceneData = new SceneData();
+
     void Awake(){
-        Assert.IsNotNull(Tape);  
+        Assert.IsNotNull(Tape);
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
-    void Start()
-
-    {
-        
-    }
-
-   
+    
     void Update()
     {
        Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
+        DisplayHudData();
+    }
+
+    public void IncrementCoinCount()
+    {
+        sceneData.coinCount++;
+    }
+
+    void DisplayHudData()
+    {
+        uiComponents.hud.txtCoinCount.text = "X " + sceneData.coinCount;
     }
 }
