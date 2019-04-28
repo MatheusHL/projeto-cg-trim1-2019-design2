7b8c84bf5679e70e4002b59dc6fa700dedd1f5b1
MatheusHL <matheuscamposhl@gmail.com>
2019-04-28 16:45:02 -0300

    Matando o player por encostar no canto esquerco Forbidden

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other){
-        if (other.gameObject.CompareTag("Coin")){
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Coin")) {
             SFXManager.instance.ShowCoinParticles(other.gameObject);
             AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
             LevelManager.instance.IncrementCoinCount();
         }
         else if (other.gameObject.layer == LayerMask.NameToLayer("Enemies")) {
-            Camera.main.GetComponentInChildren<AudioSource>().mute = true;
-            LevelManager.instance.SetTapeSpeed(0);
-            AudioManager.instance.PlaySoundFail(gameObject);
-            SFXManager.instance.ShowDieParticles(gameObject);
-            Destroy(gameObject);
+            KillPlayer();
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Forbidden")) {
+            KillPlayer();
         }
     }
 
-
+    void KillPlayer() {
+        Camera.main.GetComponentInChildren<AudioSource>().mute = true;
+        LevelManager.instance.SetTapeSpeed(0);
+        AudioManager.instance.PlaySoundFail(gameObject);
+        SFXManager.instance.ShowDieParticles(gameObject);
+        Destroy(gameObject);        
+    }
 
 }
