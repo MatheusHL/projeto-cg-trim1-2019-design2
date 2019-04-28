6a838aa75e04b9ac1dbc2f489c77884505db8bcb
MatheusHL <matheuscamposhl@gmail.com>
2019-04-28 17:57:48 -0300

    LevelCompletePanel

--- Assets/Scripts/UIComponents.cs
+++ Assets/Scripts/UIComponents.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿    
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -7,15 +8,26 @@ using UnityEngine.UI;
 [Serializable]
 public class UIComponents 
 {
-   [Serializable]
-
+    [Serializable]
     public class Hud {
 
         [Header("Text")]
-       public Text txtCoinCount; 
-               
-       [Header("Other")] 
-       public GameObject panelHud;
-    }    
+        public Text txtCoinCount;
+
+        [Header("Other")]
+        public GameObject panelHud;
+    }
+
+    [Serializable]
+    public class LevelCompletePanel {
+
+        [Header("Text")]
+        public Text txtScore;
+
+        [Header("Other")]
+        public GameObject LCPanel;
+    }
+
     public Hud hud;
+    public LevelCompletePanel levelCompletePanel;
 }
